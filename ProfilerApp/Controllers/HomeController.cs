94731aab00e2416737458c4405b62a02a7118c10
Saba <smeraj121@gmail.com>
2018-09-21 15:53:58 +0530

    App push to git

---
+++ ProfilerApp/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System.Web.Mvc;
+
+namespace ProfilerApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Index
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult AddFAQ(FAQuestions question)
+        //{
+        //    int result = crudService.AddFAQ(question);
+        //    if (result > 0)
+        //    {
+        //        TempData["Success"] = "Successfully Added";
+        //        return RedirectToAction("Edit", new { id = result });
+        //    }
+        //    else
+        //    {
+        //        ViewBag.Success = "Attempt failed";
+        //        return View();
+        //    }
+        //}
+    }
+}
---
+++ ProfilerApp/Models/Users.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ProfilerApp.Models
+{
+    public class Users
+    {
+        [Key]
+        public int UserID { get; set; }
+
+        [Required]
+        [StringLength(50, ErrorMessage = "Word limit is 50 characters.")]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(20, ErrorMessage = "Word limit is 20 characters.")]
+        public string Password { get; set; }
+
+        [Required]
+        [StringLength(20, ErrorMessage = "Word limit is 20 characters.")]
+        public int Username { get; set; }
+
+        [Required]
+        public int Name { get; set; }
+
+        
+        public string Gender { get; set; }
+
+        [StringLength(100, ErrorMessage = "Word limit is 100 characters.")]
+        [Display(Name = "BirthDay")]
+        public DateTime DOB { get; set; }
+    }
+}
